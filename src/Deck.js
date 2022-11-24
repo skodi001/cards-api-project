a3914fb027fa3a2eafd5e634605d29d5b8d65530
Shokhmurod <shokhpjwstk@gmail.com>
2022-11-24 02:12:14 +0100

    updated the style with transformation randomy. Styled cards and deck

--- src/Deck.js
+++ src/Deck.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import Card from "./Card";
 import "./Deck.css";
 import axios from "axios";
-
 const API_BASE_URL = "https://www.deckofcardsapi.com/api/deck";
 
 class Deck extends Component {
@@ -11,21 +10,18 @@ class Deck extends Component {
     this.state = { deck: null, drawn: [] };
     this.getCard = this.getCard.bind(this);
   }
-
   async componentDidMount() {
     let deck = await axios.get(`${API_BASE_URL}/new/shuffle/`);
     this.setState({ deck: deck.data });
   }
-
   async getCard() {
-    let id = this.state.deck.deck_id;
+    let deck_id = this.state.deck.deck_id;
     try {
-      let cardURL = `${API_BASE_URL}/${id}/draw/`;
-      let cardRes = await axios.get(cardURL);
-      if (!cardRes.data.cards[0]) {
-        throw new Error("No cards remaining!");
+      let cardUrl = `${API_BASE_URL}/${deck_id}/draw/`;
+      let cardRes = await axios.get(cardUrl);
+      if (!cardRes.data.success) {
+        throw new Error("No card remaining!");
       }
-
       let card = cardRes.data.cards[0];
       console.log(cardRes.data);
       this.setState((st) => ({
@@ -48,13 +44,17 @@ class Deck extends Component {
       <Card key={c.id} name={c.name} image={c.image} />
     ));
     return (
-      <div>
-        <h1>Card Dealer</h1>
-        <button onClick={this.getCard}>Get Card!</button>
+      <div className="Deck">
+        <h1 className="Deck-title">♦ Card Dealer ♦</h1>
+        <h2 className="Deck-title subtitle">
+          ♦ A little demo made with React ♦
+        </h2>
+        <button className="Deck-btn" onClick={this.getCard}>
+          Get Card!
+        </button>
         <div className="Deck-cardarea">{cards}</div>
       </div>
     );
   }
 }
-
 export default Deck;
